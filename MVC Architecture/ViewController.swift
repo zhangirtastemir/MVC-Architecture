3633d041b6589a03c5f05becfee4f74d6ff62e78
Zhangirkhan <zhangirtastemir@github.com>
2022-08-15 19:03:35 +0600

    UIAlertContoller

--- MVC Architecture/ViewController.swift
+++ MVC Architecture/ViewController.swift
@@ -8,12 +8,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var hiLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    @IBAction func showButton(_ sender: Any) {
+      
+        self.hiLabel.text = "Hi, "
+        self.alert(title: "Внимание!", message: "Введите ваше имя", style: .alert)
+    }
+    
+    //2
+    func alert(title: String, message: String, style: UIAlertController.Style) {
+        let alertController = UIAlertController(title: title, message: message , preferredStyle: style)
+        let action = UIAlertAction(title: "ok", style: .default) { (action) in
+            let text = alertController.textFields?.first
+            self.hiLabel.text! += (text?.text!)! + ("!")
+        
+        }
+        
+        alertController.addTextField{ (textField) in
+            textField.isSecureTextEntry = true
+        }
+        alertController.addAction(action)
+        self.present(alertController, animated: true, completion: nil)
+        
+        }
+    }
+    
 
-
-}
-
+ 
